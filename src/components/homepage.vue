c99d482933ca8a52c3dd6e5123f17077a929f30a
Adrian Luces <adrianluces111@gmail.com>
2021-02-02 20:02:23 -0400

    homepage updated

--- src/components/homepage.vue
+++ src/components/homepage.vue
@@ -1,7 +1,34 @@
 <template >
-    <div>
-        
-    </div>
+    <v-container fluid>
+        <v-card>
+            <v-card-title
+                class="justify-center"
+            >
+                ORDENA TU PIZZA!!!
+            </v-card-title>
+
+            <v-card-text
+                class="justify-center"
+            >
+                <h1 class="justify-center"> Disfruta del verdadero sabor de la Nonna con Pizzas DANG</h1>
+                <p> DANG that's tasty </p>
+            </v-card-text>
+            <v-card-actions
+                class="justify-center"
+            >
+                <v-btn
+                    x-large
+                    color="#F4511E"
+                    elevation="20"
+                >
+                <span>
+                    ORDENA YA !!!
+                </span>
+
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-container>
 </template>
 
 <script>
