3a57e327a07e10ddb18a6aa3c4419371daa9418a
Adrian Luces <adrianluces111@gmail.com>
2021-02-03 22:41:48 -0400

    Toppings table

--- src/components/salessxsize.vue
+++ src/components/salessxsize.vue
@@ -1,6 +1,26 @@
 <template >
     <v-container fluid>
-        v-selec
+        <v-row>
+            <v-col>
+                <v-select
+                    v-model="sizes"
+                    outlined
+                    label="Tamaños"
+                    :items="szs"
+                >
+
+                </v-select>
+            </v-col>
+            <v-col
+                class="justify-left"
+            >
+                <v-btn
+                    @click="seekResults"
+                >
+                    <span> Buscar </span>
+                </v-btn>
+            </v-col>
+        </v-row>
         <v-data-table
             :headers="headers"
         >
@@ -13,17 +33,27 @@ export default {
     data: ()=> ({
         headers: [
             {
-                text:"Pizza Size"            
+                text:"Pizza Size",
+                value: 'pizza_size'            
             },
             {
-                text:"Toppings"
+                text:"Toppings",
+                value: 'toppings'
             },
             {
-                text:"Price"
+                text:"Price",
+                value: 'price'
             }
-        ]
+        ],
+        szs: ["Pequeña" , "Mediana" , "Grande"]
     
-    })
+    }),
+    methods: {
+        seekResults(){
+            
+        }
+
+    }
 }
 </script>
 <style >
