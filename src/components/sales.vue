59ddb8416b63a2b3fcdb10a0f78cdcd3a66c045a
Adrian Luces <adrianluces111@gmail.com>
2021-02-04 01:28:41 -0400

    Update resumenes de venta

--- src/components/sales.vue
+++ src/components/sales.vue
@@ -10,9 +10,55 @@
         <v-data-table
             :headers="headers"
             :items="dataSource"
+            v-model="selected"
+            item-key="id"
             
+             
         >
+            <template v-slot:item.action="{}">
+                <v-btn
+                    @click.stop="dialog = true"
+                    
+                >
+                    VER
+                </v-btn>
+                <v-dialog
+                    v-model="dialog"
+                    max-width="800"
+                    hide-overlay
+                    elevation="0"
+                >
+                    <v-container fluid>
+                        <v-card >
+                            <v-row>
+                                <v-col>
+                                    <v-card-title class="headline justify-center">Detalle de Orden</v-card-title>
+                                    
+                                </v-col>
+                            </v-row>
+                            <v-card>
+                                <v-data-table
+                                    :headers="mdheaders"
+                                >
+                                    </v-data-table>
+                                </v-card>
+                            <v-card-actions class="justify-center">
+                                <v-spacer></v-spacer>
+                                <v-btn
+                                    color="#43A047"
+                                    @click="seekDetail()"
 
+                                >
+                                    UPDATE
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
+
+                        
+                    </v-container>
+                </v-dialog>
+            </template>
+            
         </v-data-table>
     </v-container>
 </template>
@@ -39,10 +85,32 @@ export default {
             {
                 text:"Date",
                 value:'order_date'
+            },
+            {
+                text:"Detalle" ,
+                value: 'action'
+            }
+        ],
+        mdheaders: [
+            {
+                text: "Size",
+                value: 'pizza_size' ,
+            },
+            {
+                text:"Toppings",
+                value:'toppings'
+            },
+            {
+                text:"Price",
+                value:'price'
             }
         ],
         processed:[],
         dataSource: [],
+        dialog:false,
+        mdataSource:[],
+        id_detail: null,
+        selected:null
 
     
     }),
@@ -55,10 +123,17 @@ export default {
                     console.log(this.dataSource)
                 });
 
-            
-            
+        },
 
+        seekDetail(){
+            this.mdataSource = [];
+            this.processed = [];
+            console.log(this.selected)
 
+            serv.getOrderDetail(this.selected)
+                .then(response => {
+                    this.mdataSource = JSON.parse(JSON.stringify(response.data.pizzas));
+                });
         }
 
     },
